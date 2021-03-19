a55a6f3ac3e37cbf76770290d298f5981cfc34cd
aksahy sangani <asangani705@rku.ac.in>
2021-03-20 02:24:36 +0530

    change index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 
-mongoose.connect("mongodb://localhost:27017/dbForNode",{useNewUrlParser:true,useUnifiedTopology: true}).then(
+mongoose.connect("mongodb+srv://User:akshay0240@cluster0.5royq.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology: true}).then(
     ()=>{
         const app = express();
         app.use("/api",route);
