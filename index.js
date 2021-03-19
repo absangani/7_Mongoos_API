b89d5f80cf0630e2a7c3f8a874debfae64e4c53c
aksahy sangani <asangani705@rku.ac.in>
2021-03-20 03:30:41 +0530

    add comments

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const bodyparser = require('body-parser')
 const mongoose = require('mongoose');
 const route = require('./route');
 
+// connection url 2 type
+// 1: local mongo db database than mongodb://localhost:27017/<database name>
+// -> run on 3000 port
+// 2: MLAB databse than connection url is mongodb+srv://User:akshay0240@cluster0.5royq.mongodb.net/student?retryWrites=true&w=majority
+// requrired line 18 port process.env.PORT 
+// in packeage.js start file name
+//and at last heroku deployment 
+
 mongoose.connect("mongodb+srv://User:akshay0240@cluster0.5royq.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology: true}).then(
     ()=>{
         const app = express();
         app.use(bodyparser.urlencoded({extended:true}));
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.json());
         app.use("/api",route);
         app.listen(process.env.PORT || 3000,()=>{
             console.log("server stared..!!!");
